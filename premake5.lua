965cf10f83eb6cbf8e79be13b7b723d76fc7897c
Octalype <octalype@gmail.com>
2018-04-27 01:39:58 +0200

    Fix white-space in premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -3,27 +3,27 @@ cwd = os.getcwd()
 binpath = function (path) targetdir(os.realpath(("%s/bin/%s"):format(cwd, path))) end
 
 solution "Websocket"
-	configurations { "Debug", "Release" }
-	platforms { "x32", "x64" }
-	location "build"
+    configurations { "Debug", "Release" }
+    platforms { "x32", "x64" }
+    location "build"
 
-	defines {
+    defines {
         "_CRT_SECURE_NO_WARNINGS",
         "_SCL_SECURE_NO_WARNINGS",
         "_CRT_NONSTDC_NO_DEPRECATE",
-	}
+    }
 
-	filter "platforms:x32"
+    filter "platforms:x32"
         architecture "x32"
 
     filter "platforms:x64"
         architecture "x64"
 
-	flags { "C++14" }
-	pic "On"
-	symbols "On"
-	
-	filter "system:windows"
+    flags { "C++14" }
+    pic "On"
+    symbols "On"
+    
+    filter "system:windows"
         defines { "WIN32", "_WIN32" }
 
     filter "configurations:Debug"
@@ -36,4 +36,4 @@ solution "Websocket"
         flags { "C++14", "Optimize" }
         optimize "On"
 
-	include "module"
+    include "module"
